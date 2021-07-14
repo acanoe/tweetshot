f076274b286a4cfe96c7351a79048b66e8c47722
Hendika N <hendika.new@gmail.com>
2021-07-14 22:12:10 +0700

    Created a simple API

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 import os
 
+from flask import Flask, request, make_response
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
-url = "https://twitter.com/hincapandjaitan/status/1414899584619319306"
+app = Flask(__name__)
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--hide-scrollbars")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+def get_snap(url):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--hide-scrollbars")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-with webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options) as driver:
-    # get url
-    driver.get(url)
+    with webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options) as driver:
+        # get url
+        driver.get(url)
 
-    # select elements
-    tweet = WebDriverWait(driver, timeout=20).until(lambda d: d.find_element_by_tag_name("article").find_element_by_xpath(".."))
-    banner = driver.find_element_by_id("layers")
-    action_icons = driver.find_element_by_xpath('//div[@role="group"]')
-    more_icons = driver.find_element_by_xpath('//div[@data-testid="tweet"]/div[2]/div/div/div/div[2]')
+        # select elements
+        tweet = WebDriverWait(driver, timeout=20).until(lambda d: d.find_element_by_tag_name("article").find_element_by_xpath(".."))
+        banner = driver.find_element_by_id("layers")
+        action_icons = driver.find_element_by_xpath('//div[@role="group"]')
+        more_icons = driver.find_element_by_xpath('//div[@data-testid="tweet"]/div[2]/div/div/div/div[2]')
 
-    # remove unnecessary icons
-    elements_to_remove = [banner, action_icons, more_icons]
-    for element in elements_to_remove:
-        driver.execute_script("arguments[0].setAttribute('style','display: none;')", element)
+        # remove unnecessary icons
+        elements_to_remove = [banner, action_icons, more_icons]
+        for element in elements_to_remove:
+            driver.execute_script("arguments[0].setAttribute('style','display: none;')", element)
 
-    # take a screenshot
-    tweet.screenshot("{}-{}.png".format(url.split('//')[-1].split('.')[0], url.split('/')[3]))
+        # take a screenshot
+        driver.set_window_size(driver.get_window_size().get("width"), tweet.size["height"] + 250)
+        return tweet.screenshot_as_png
 
-    print("Finished!")
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
+@app.route("/snap")
+def get_snapshot():
+    url = request.args.get('url')
+    if url is not None and url.startswith("https://twitter.com/"):
+        image_binary = get_snap(url)
+        response = make_response(image_binary)
+        response.headers.set('Content-Type', 'image/png')
+        response.headers.set(
+            'Content-Disposition',
+            'attachment',
+            filename="{}-{}.png".format(url.split('/')[3], url.split('/')[5]))
+        return response
+    else:
+        return {
+            "status": "error",
+            "message": "Please provide a valid Twitter URL"
+        }
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
